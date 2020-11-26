60a329823d3217cc1f090fea2f3ea3259d7f91f4
SivakovMax <littlelittledevilmax@gmail.com>
2020-11-26 14:47:31 +0200

    added js, styles, html

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,8 @@
-$(function(){
+$(function () {
 
-
+  document.getElementById('user__nav-search').onclick = function () {
+    var el = document.getElementById('user__nav__form');
+    el.style.visibility === 'hidden' ? el.style.visibility = 'visible' : el.style.visibility = 'hidden';
+  }
+ 
 });
